92229651b0913009733604dd922a6bd8b5caa31b
kouhei yamanaka <home3751@gmail.com>
2021-01-26 18:36:04 +0900

    indexページ作成

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,27 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head> 
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
 
+//役割：予めgetStaticPropsにidを渡す⇒ビルド時に渡しておかないと、静的生成が出来ない為
 export async function getStaticPaths() {
 
 //{id:ファイル名}のオブジェクトの配列を返す
@@ -26,7 +35,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
 
 //idと解析されたcontentを返す関数
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
